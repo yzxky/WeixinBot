3d2e1955ad50e9fdbe42b12aa009ef8d72547191
Shaoming-Song <sz941023@sina.com>
2017-02-16 14:35:34 +0800

    0216 update

---
+++ id_group.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+#-*-coding:utf-8-*-
+id_dict = {
+	u"xky":'0',
+	u"ssm":'1',
+	u"ly":'2',
+	u"hjf":'3',
+	u"gjq":'4',
+	u"gdx":'5',
+	u"zrb":'6',
+	u"hxf":'7',
+	u"yc":'8',
+	u"lsy":'9',
+	u"test":'10',
+	u"ldy":'11',
+	u"wjl":'12',
+	u"wj":'13',
+}
